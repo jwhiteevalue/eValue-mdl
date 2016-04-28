7a6e41cdd2bb18f6ac93464ea2ae7465e7651315
jwhiteevalue <jon.white@evalueis.com>
2016-04-28 11:10:19 +0100

    update to ripples.js - will it work in a modal?
    Signed-off-by: jwhiteevalue <jon.white@evalueis.com>

--- js/ripple.js
+++ js/ripple.js
@@ -1,53 +1,70 @@
-(function(window, $) {
-  $(function() {
-		$('.btn').on('click', function(event) {
-      var $div = $('<div/>'),
-        btnOffset = $(this).offset(),
-        xPos = event.pageX - btnOffset.left,
-        yPos = event.pageY - btnOffset.top;
+function ripplesEverywhere() {
 
-      $div.addClass('ripple-effect');
-      var $ripple = $('.ripple-effect');
+	(function(window, $) {
+	  $(function() {
+			$('.btn').on('click', function(event) {
+	      var $div = $('<div/>'),
+	        btnOffset = $(this).offset(),
+	        xPos = event.pageX - btnOffset.left,
+	        yPos = event.pageY - btnOffset.top;
 
-      $ripple.css('height', $(this).height());
-      $ripple.css('width', $(this).height());
-      $div
-        .css({
-          top: yPos - ($ripple.height() / 2),
-          left: xPos - ($ripple.width() / 2),
-          background: $(this).data('ripple-color')
-        })
-        .appendTo($(this));
+	      $div.addClass('ripple-effect');
+	      var $ripple = $('.ripple-effect');
 
-      window.setTimeout(function() {
-        $div.remove();
-      }, 2000);
-    });
+	      $ripple.css('height', $(this).height());
+	      $ripple.css('width', $(this).height());
+	      $div
+	        .css({
+	          top: yPos - ($ripple.height() / 2),
+	          left: xPos - ($ripple.width() / 2),
+	          background: $(this).data('ripple-color')
+	        })
+	        .appendTo($(this));
 
-		$('.nav a').on('click', function(event) {
-			var $div = $('<div/>'),
-				btnOffset = $(this).offset(),
-				xPos = event.pageX - btnOffset.left,
-				yPos = event.pageY - btnOffset.top;
+	      window.setTimeout(function() {
+	        $div.remove();
+	      }, 2000);
+	    });
 
-			$div.addClass('ripple-effect');
-			var $ripple = $('.ripple-effect');
+			$('.nav a').on('click', function(event) {
+				var $div = $('<div/>'),
+					btnOffset = $(this).offset(),
+					xPos = event.pageX - btnOffset.left,
+					yPos = event.pageY - btnOffset.top;
 
-			$ripple.css('height', $(this).height());
-			$ripple.css('width', $(this).height());
-			$div
-				.css({
-					top: yPos - ($ripple.height() / 2),
-					left: xPos - ($ripple.width() / 2),
-					background: $(this).data('ripple-color')
-				})
-				.appendTo($(this));
+				$div.addClass('ripple-effect');
+				var $ripple = $('.ripple-effect');
 
-			window.setTimeout(function() {
-				$div.remove();
-			}, 2000);
-		});
+				$ripple.css('height', $(this).height());
+				$ripple.css('width', $(this).height());
+				$div
+					.css({
+						top: yPos - ($ripple.height() / 2),
+						left: xPos - ($ripple.width() / 2),
+						background: $(this).data('ripple-color')
+					})
+					.appendTo($(this));
 
-  });
+				window.setTimeout(function() {
+					$div.remove();
+				}, 2000);
+			});
 
-})(window, jQuery);
+	  });
+
+	})(window, jQuery);
+)};
+/* Fire up MDL in doc load and a modal */
+(function ripplesGoPower(){
+	'use strict';
+		console.info("ripplesGoPower()");
+	$(document).ready(function ripplesOnDocumentReady() {
+			console.info("calling on document ready");
+	  ripplesEverywhere();
+	});
+
+	$(".modal[role='dialog']").on('loaded.bs.modal', function ripplesOnModal() {
+			console.info("calling on a modal loaded");
+		ripplesEverywhere();
+	})
+})();
